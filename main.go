f0a7a08b52d5e976d7926759f9f6dd2796b1f6ce
Gorkem <72773406+Trou95@users.noreply.github.com>
2024-08-28 14:26:20 +0300

    Update main.go

--- main.go
+++ main.go
@@ -16,18 +16,17 @@ import (
 var db *sql.DB
 
 type User struct {
-	ID       int
-	Username string
-	Password string `json:"-"` 
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"-"`
 }
 
 type LoginDTO struct {
-	Username string    `json:"username"`
+	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
 func main() {
-	
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	user := os.Getenv("DB_USER")
@@ -38,6 +37,7 @@ func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		host, port, user, password, dbname, sslmode)
 
+	var err error
 	db, err = sql.Open("postgres", psqlInfo)
 	if err != nil {
 		log.Fatal(err)
@@ -52,38 +52,36 @@ func main() {
 	fmt.Println("Başarıyla veritabanına bağlanıldı!")
 
 	router := mux.NewRouter()
-
 	router.HandleFunc("/auth", login).Methods("POST")
 
-	
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-    var info LoginDTO
-    err := json.NewDecoder(r.Body).Decode(&info)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+	var info LoginDTO
+	err := json.NewDecoder(r.Body).Decode(&info)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-    var user User
-    err = db.QueryRow("SELECT id, username, password FROM users WHERE username = $1", info.Username).Scan(&user.ID, &user.Username, &user.Password)
-    if err != nil {
-        if err == sql.ErrNoRows {
-            http.Error(w, "Invalid Username or Password", http.StatusNotFound)
-        } else {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-        }
-        return
-    }
+	var user User
+	err = db.QueryRow("SELECT id, username, password FROM users WHERE username = $1", info.Username).Scan(&user.ID, &user.Username, &user.Password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, "Invalid Username or Password", http.StatusNotFound)
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
 
-    err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(info.Password))
-    if err != nil {
-        http.Error(w, "Invalid Username or Password", http.StatusUnauthorized)
-        return
-    }
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(info.Password))
+	if err != nil {
+		http.Error(w, "Invalid Username or Password", http.StatusUnauthorized)
+		return
+	}
 
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(user)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
 }
